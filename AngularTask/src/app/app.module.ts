012324a0c2856b1ddcb8a836f6f52d8c84134cce
Arkant <pan_rostislav@ukr.net>
2018-10-07 10:39:34 +0300

    Styles empty components

--- AngularTask/src/app/app.module.ts
+++ AngularTask/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contact/contacts/contacts.component';
+import { ListComponent } from './contact/list/list.component';
+import { FormsModule } from '@angular/forms';
+// import { Route } from '../../node_modules/@angular/compiler/src/core';
+
+// const routes: Route = [
+//   {path: }
+// ]forms
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
